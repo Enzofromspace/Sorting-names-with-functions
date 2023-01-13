21864a8f1163db232f1aa6a72bf6cb2f2a3dcb5e
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-13 11:00:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sorting-names-with-functions
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yf1js1)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Sorting Names with JS</h1>`;
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
