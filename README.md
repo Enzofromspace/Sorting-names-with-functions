236fed8d73d531d4f8a4626363aac6f83981f2af
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-13 11:20:43 +0300

    Got strip function working

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Sorting-names-with-functions
 
+Sorting strings of bandnames with JS functions. 
+From Javascript30 by Wes Bos.
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yf1js1)
--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Sorting Names with JS</h1>`;
 
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+ function strip(bandName){
+   return bandName.replace(/^(a |the |an )/i, '').trim();
+ }
+
+const sortedBands = bands.sort(function(a,b){
+  if(a>b){
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.log(strip('an old dog'));
